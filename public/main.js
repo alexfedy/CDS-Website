cf7d2e34e55be788e704ddcf84a339677ef00c85
alexfedy <916falex@gmail.com>
2021-12-22 12:58:09 -0800

    first commit

---
+++ public/main.js
@@ -0,0 +1,75 @@
+//javascript for toggle menu
+var navLinks = document.getElementById("navLinks");
+var link = document.querySelectorAll(".nav-links ul li a");
+link.forEach((item) => {
+  item.addEventListener("click", () => {
+    navLinks.style.right = "-200px";
+  });
+});
+
+document.getElementById("close").addEventListener("click", () => {
+  navLinks.style.right = "0";
+});
+document.getElementById("burger").addEventListener("click", () => {
+  navLinks.style.right = "-200px";
+});
+
+//scroll animations
+window.sr = ScrollReveal();
+sr.reveal(".animate-infirst", {
+  origin: "center",
+  duration: 2500,
+  distance: "0rem",
+  delay: 400,
+});
+sr.reveal(".animate-in", {
+  origin: "center",
+  duration: 1500,
+  distance: "0rem",
+  delay: 2000,
+});
+
+//scroll to top on reload
+if (history.scrollRestoration) {
+  history.scrollRestoration = "manual";
+} else {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+}
+
+//form submission to server
+$("form").on("submit", (e) => {
+  e.preventDefault();
+  const uname = $("#uname").val().trim();
+  const uemail = $("#uemail").val().trim();
+  const udate = $("#udate").val().trim();
+  const utime = $("#utime").val().trim();
+  const utext = $("#utext").val().trim();
+  const uurl = $("#uurl").val().trim();
+  const data = {
+    uname,
+    uemail,
+    udate,
+    utime,
+    utext,
+    uurl,
+  };
+  $.post("/email", data, function () {
+    console.log("Server received data");
+  });
+
+  //reload page on submission
+  document.getElementById("btn").innerHTML = "Sent!";
+  setTimeout(() => {
+    document.getElementById("form").style.display = "none";
+  }, 2000);
+  setTimeout(() => {
+    document.getElementById("sent-message").style.display = "block";
+  }, 3000);
+  // setTimeout(reload, 2000);
+  // function reload() {
+  //   window.location.reload();
+  //   window.scrollTop(0);
+  // }
+});
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const path = require("path");
+const nodemailer = require("nodemailer");
+const helmet = require("helmet");
+const email_user = process.env.EMAILUSER;
+const email_pass = process.env.EMAILPASS;
+//data parsing
+app.use(helmet());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: [
+        "'self'",
+        "cdnjs.cloudflare.com",
+        "fonts.gstatic.com",
+        "fonts.googleapis.com",
+      ],
+      scriptSrc: [
+        "'self'",
+        "unpkg.com",
+        "ajax.googleapis.com",
+        "'unsafe-inline'",
+      ],
+      fontSrc: [
+        "'self'",
+        "cdnjs.cloudflare.com",
+        "fonts.gstatic.com",
+        "fonts.googleapis.com",
+      ],
+    },
+  })
+);
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/cdsimages", express.static(__dirname + "public/cdsimages"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: false,
+//   })
+// );
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.post("/email", (req, res) => {
+  //send email
+  const JSONdata = JSON.stringify(req.body);
+  const info = JSON.parse(JSONdata);
+  if (info.uurl != "") {
+    return;
+  }
+  const output = `<h3>New Schedule Request</h3>
+    <h4>Name: </h4><p>${info.uname}</p>
+    <h4>Email: </h4><p>${info.uemail}</p>
+    <h4>Date: </h4><p>${info.udate}</p>
+    <h4>Time: </h4><p>${info.utime}</p>
+    <h4>Info: </h4><p>${info.utext}</p>`;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: email_user, // generated ethereal user
+      pass: email_pass, // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"CDS Mail Contact" <techalex330@gmail.com>', // sender address
+    to: "916falex@gmail.com", // list of receivers
+    subject: "Schedule Request", // Subject line
+    text: "output", // plain text body
+    html: output, // html body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Email Sent");
+    // console.log("Message sent: %s", info.messageId);
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    //   res.render('contact', {msg:'Email has been sent'});
+  });
+});
+
+app.listen(process.env.PORT || 3000);
